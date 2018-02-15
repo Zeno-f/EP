a1a2685d5ae973fc8d776a77ca827dd2aa505e06
Zeno Scheltens <zeno.scheltens@gmail.com>
2018-02-15 10:10:54 +0100

    final code for EP eerste opdracht

--- MPLABproject/main.c
+++ MPLABproject/main.c
@@ -5,9 +5,15 @@
  * Created on 15 februari 2018, 9:38
  */
 
-
 #include <xc.h>
 
-void main(void) {
-    return;
+#define _XTAL_FREQ 8000000  // X-tal = 8 MHz
+
+void main(void)
+{
+    TRISC = 0x00;
+	
+	LATC = 0xB0;
+
+	while(1);
 }
