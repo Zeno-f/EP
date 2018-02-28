e8f7f571e25ae61774139153f81e95d852b55ae5
Zeno Scheltens <zeno.scheltens@gmail.com>
2018-02-28 15:35:48 +0100

    add first version of looplicht

--- MPLABproject/main.c
+++ MPLABproject/main.c
@@ -1,19 +1,77 @@
 /*
- * File:   main.c
- * Author: Zeno
+ *	Embedded Programming
  *
- * Created on 15 februari 2018, 9:38
+ *	Opdracht: Looplicht
+ *
+ *	Code  by Zeno Scheltens
+ *
  */
 
 #include <xc.h>
 
 #define _XTAL_FREQ 8000000  // X-tal = 8 MHz
+#define button1 PORTBbits.RB0
+#define button2 PORTBbits.RB1
+
+// verschillende variabelen voor het programma
+int button1 = 0;
+int button2 = 0;
+int loop = 0;
+int loopLicht = 0;
+int ledArray[3];
+
+	ledArray[0] = 0x80;
+	ledArray[1] = 0x30;
+	ledArray[2] = 0x10;
+
+ANCON1
+
+// hiermee stel ik alleen deze twee bits in als input
+TRISBbits.RB0 = 1;
+TRISBbits.RB1 = 1;
+
+// hiermee stel ik alleen deze 3 bits als output
+TRISCbits.RC7 = 0;
+TRISCbits.RC5 = 0;
+TRISCbits.RC4 = 0;
 
 void main(void)
 {
-    TRISC = 0x00;
-	
-	LATC = 0xB0;
-
-	while(1);
+    while(1)	{
+		
+		if (PORTRB0 == 0)	{
+			button1 = 1;
+		}
+		
+		if	(PORTRB1 == 0)	{
+			button2 = 1;
+		}
+		
+		if	(button1 ^ button2)	{
+			loopLicht = 1;
+		}
+		
+		if (button1 & button2)	{
+			loopLicht = 0;
+			button1 = 0;
+			button2 = 0;
+		}
+		
+		if (loopLicht == 1)	{
+			
+			if	(loop > 2)	{
+				loop = 0;
+				LATC(ledArray[loop]);
+				loop++;
+			}
+			
+			else	{
+				LATC(ledArray[loop]);
+				loop++;
+			}
+		}
+		
+		delay(90)
+		
+	}
 }
