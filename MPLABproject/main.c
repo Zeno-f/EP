b7c761398fcc41723e9989095fb9b01b3aa78908
Zeno Scheltens <zeno.scheltens@gmail.com>
2018-03-07 16:50:19 +0100

    add the content KAPJ provided for the opdracht

--- MPLABproject/main.c
+++ MPLABproject/main.c
@@ -15,12 +15,41 @@
 
 #define _XTAL_FREQ 8000000  // X-tal = 8 MHz
 
+// shady code to be able to use printf with the microchip
+void putch (char c)	{
+	while (TXSTA1bits.TRMT == 0)	{
+		'transmit register' = c;
+	}
+}
+
 void main() {
+	
 	// Ini fase
 	
+	TRISCbits.TRISC6 = 0;	// Tx1 output
+	PIE3bits.RC2IE = 0;		// disable Rx interrupt USART2
+	PIE3bits.TX2IE = 0;		// disable Tx interrupt USART2
+	PIE1bits.RC1IE = 0;		// disable Rx interrupt USART1
+	PIE1bits.TX1IE = 0;		// disable Tx interrupt USART1
+	TXSTA1 = ????;			//
+	RCSTA1 = ????;			//
+	BAUDCON1 = ????;		//
+	SPBRG1 = ????;			//
 	
+	// port settings
+	
+	TRISC = ????;			// portC output
+	LATC = ????;			// LED1 groen aan
+	
+	// variables 
+	
+	int ????;
 	
 	// main fase
 	
-	
+	while (1)	{
+		for ('range aangeven')	{
+			printf("test = %d\n\r", ????);
+		}
+	}	
 }
