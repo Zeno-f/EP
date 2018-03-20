4b610ca7c348a97a5e5cd51f887c85f68c9ee255
Zeno Scheltens <zeno.scheltens@gmail.com>
2018-03-20 13:33:44 +0100

    add communication things

--- MPLABproject/main.c
+++ MPLABproject/main.c
@@ -16,4 +16,16 @@
 #define _XTAL_FREQ 8000000  // X-tal = 8 MHz
 
 void main() {
+	
+	// Ini fase
+	
+	TRISCbits.TRISC6 = 0;	// Tx1 output
+	PIE3bits.RC2IE = 0;		// disable Rx interrupt USART2
+	PIE3bits.TX2IE = 0;		// disable Tx interrupt USART2
+	PIE1bits.RC1IE = 0;		// disable Rx interrupt USART1
+	PIE1bits.TX1IE = 0;		// disable Tx interrupt USART1
+	TXSTA1 = 0xA0;			//
+	RCSTA1 = 0x80;			//
+	BAUDCON1 = 0xC0;		//
+	SPBRG1 = 12;			//  
 }
