51c06381ab7c330e6eb16983cff62dfd028e13f1
Zeno Scheltens <zeno.scheltens@gmail.com>
2018-03-27 12:17:01 +0200

    kit yi basis versie

--- MPLABproject/main.c
+++ MPLABproject/main.c
@@ -15,6 +15,14 @@
 
 #define _XTAL_FREQ 8000000  // X-tal = 8 MHz
 
+int Analog_Digital_convertor(void);
+
+void putch (char c)
+{
+    while(TXSTA1bits.TRMT == 0);
+    TXREG1 = c;
+}
+
 void main() {
 	
 	// Ini fase
@@ -28,4 +36,23 @@ void main() {
 	RCSTA1 = 0x80;			//
 	BAUDCON1 = 0xC0;		//
 	SPBRG1 = 12;			//  
+	
+	ANCON0.ANSEL1 = 1;
+    ANCON1.ANSEL9 = 1;
+    
+    while(1)
+    {
+        value = _Analog_Digital_convertor();
+        printf("adcValue = %d\n\r", value);
+    }
+	
+	int Analog_Digital_convertor(void)
+    {
+        ADCON0 = 00100111;       //channel AN1[pin3](bit6-2), Start ADCconversion(bit1), ADC on(bit0)
+        ADCON1 = 00000010;       //trigger ECCP1(bit7-6), AVdd(bit5-4), AVss(bit3), Neg Channel00(AVss)(bit2-0)
+        ADCON2 = 10110001;       //right justified(bit7), Tad 0(bit5-3), conversion CLK Fosc/2(bit2-0)
+        while ( ADCON0bits.bit1 == 1);
+        return ADRESH << 8| ADRESL;
+    }
+
 }
