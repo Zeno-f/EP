0915643db722357af41e948fe7714bdadf230a8e
Zeno Scheltens <zeno.scheltens@gmail.com>
2018-04-02 18:42:40 +0200

    add ADC interrupt

--- MPLABproject/main.c
+++ MPLABproject/main.c
@@ -46,7 +46,12 @@ void interrupt myIsr (void)	{
 
 //low priority interrupts
 void interrupt low_priority moreIsr (void)	{
-	;  // 
+	
+	// interrupt becouse the ADC is done
+	if (ADCON0bits.ADON && PIRbits.ADIF)	{
+		PIRbits.ADIF = 0;								// reset ADC interrupt  
+		;												// empty, this interrupt is supposed to happen inside the function that calls the ADC
+	}
 }
 
 void main() {
@@ -93,19 +98,21 @@ void main() {
 // functie om AN1 uit te lezen
 int _Analog_Digital_convertor_AN1(void)
 {
-    ADCON0 = 0b00000111;       //channel AN1[pin3](bit6-2), Start ADCconversion(bit1), ADC on(bit0)
-    ADCON1 = 0b00110000;       //trigger ECCP1(bit7-6), AVref 4.1V(bit5-4), AVss(bit3), Neg Channel00(AVss)(bit2-0)
-    ADCON2 = 0b10110001;       //right justified(bit7), Tad 16(bit5-3), conversion CLK Fosc/8(bit2-0)
-    while ( ADCON0bits.nDONE == 1);
+    ADCON0 = 0b00000111;       	//channel AN1[pin3](bit6-2), Start ADCconversion(bit1), ADC on(bit0)
+    ADCON1 = 0b00110000;       	//trigger ECCP1(bit7-6), AVref 4.1V(bit5-4), AVss(bit3), Neg Channel00(AVss)(bit2-0)
+    ADCON2 = 0b10110001;       	//right justified(bit7), Tad 16(bit5-3), conversion CLK Fosc/8(bit2-0)
+	;							// put microcontroller to sleep
+    while (ADCON0bits.nDONE == 1);
     return ADRESH << 8| ADRESL;
 }
 
 // functie om AN9 uit te lezen
 int _Analog_Digital_convertor_AN9(void)
 {
-    ADCON0 = 0b00100111;       //channel AN1[pin3](bit6-2), Start ADCconversion(bit1), ADC on(bit0)
-    ADCON1 = 0b00110000;       //trigger ECCP1(bit7-6), AVref 4.1V(bit5-4), AVss(bit3), Neg Channel00(AVss)(bit2-0)
-    ADCON2 = 0b10110001;       //right justified(bit7), Tad 16(bit5-3), conversion CLK Fosc/8(bit2-0)
-    while ( ADCON0bits.nDONE == 1);
+    ADCON0 = 0b00100111;       	//channel AN1[pin3](bit6-2), Start ADCconversion(bit1), ADC on(bit0)
+    ADCON1 = 0b00110000;       	//trigger ECCP1(bit7-6), AVref 4.1V(bit5-4), AVss(bit3), Neg Channel00(AVss)(bit2-0)
+    ADCON2 = 0b10110001;       	//right justified(bit7), Tad 16(bit5-3), conversion CLK Fosc/8(bit2-0)
+	;							// put microcontroller to sleep
+    while (ADCON0bits.nDONE == 1);
     return ADRESH << 8| ADRESL;
 }
